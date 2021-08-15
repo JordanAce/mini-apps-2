85f07403058646b88e6caa555ca6240b2bf62105
Jordan Acevedo <jordanace83@gmail.com>
2021-08-15 14:25:42 -0500

    Got Environment Set Up

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,13 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
 
+const App = () => {
+  return (
+    <div>
+      <h3>LETS BOWL</h3>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
 
